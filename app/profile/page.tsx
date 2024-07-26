bc4db8288910faf95d8de2f0e3e45922b7bbdebd
Explorarthecreator <fredadejiemma99@gmail.com>
2024-07-26 15:34:06 +0100

    Completion of the profile page ui. No functionality added yet. Next the funtionality

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,25 @@
+import ProfileForm from "./ProfileForm";
 
 
 export default function Profile() {
   return (
-    <div>page</div>
+    <div className=" p-4 lg:p-7 ">
+      <div className="gap-4 flex flex-col bg-[#ffffff] p-5 rounded-xl min-h-screen ">
+        <div className="flex flex-col gap-3">
+          <h1 className=" text-2xl text-[#333333] font-bold">
+            Profile Details
+          </h1>
+
+          <p className="text-[#737373] text-base font-normal">
+            Add your details to create a personal touch to your profile.
+          </p>
+        </div>
+
+        <div>
+          <ProfileForm/>
+        </div>
+      </div>
+    </div>
+    
   )
 }
